78aae4431643dafdd597b4da4e78eb275bb058b9
Pia Flechsberger <pia.2002@aol.de>
2024-02-27 12:08:11 +0000

    implemented usecase1

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -72,9 +72,8 @@ public class App
         }
     }
 
-
-
-   public ArrayList<Country> getUseCase1(String continent)
+    //Usecase 1
+    public ArrayList<Country> getUseCase1(String continent)
     {
         try
         {
@@ -83,10 +82,10 @@ public class App
 
             // Create string for SQL statement
             String strSelect =
-                 "SELECT country.name, country.continent, country.population "
-                + "FROM country "
-                         + "WHERE country.continent = '" + continent + "' "
-                         + "ORDER BY country.population DESC"; //sort from the largest to the smallest
+                    "SELECT country.name, country.continent, country.population "
+                            + "FROM country "
+                            + "WHERE country.continent = '" + continent + "' "
+                            + "ORDER BY country.population DESC"; //sort from the largest to the smallest
 
             // Execute SQL statement
             ResultSet rset = stmt.executeQuery(strSelect);
@@ -107,107 +106,9 @@ public class App
         catch (Exception e)
         {
             System.out.println(e.getMessage());
-            System.out.println("Failed to get all the countries in a continent organised by largest population to smallest");
+            System.out.println("Failed to get all the countries in a continent organised by largest population to smallest.");
             return null;
         }
-    }
-
-    public ArrayList<Country> getUseCase5()
-    {
-        try
-        {
-            // Create an SQL statement
-            Statement stmt = con.createStatement();
-
-            // Create string for SQL statement
-            String strSelect =
-                    "SELECT country.name, country.population "
-                            + "FROM country "
-                            + "ORDER BY country.population ASC";
-
-            // Execute SQL statement
-            ResultSet rset = stmt.executeQuery(strSelect);
-
-            // Return new country if valid.
-            // Check one is returned
-            ArrayList<Country> country5 = new ArrayList<Country>();
-            while (rset.next())
-            {
-                Country country = new Country();
-                country.name = rset.getString("name");
-                country.population = rset.getInt("population");
-                country5.add(country);
-            }
-            return country5;
-        }
-        catch (Exception e)
-        {
-            System.out.println(e.getMessage());
-            System.out.println("Failed to get all the countries in a continent organised by largest population to smallest");
-            return null;
-        }
-    }
-
-    /*public Country getUseCase5()
-    {
-
-
-        try
-        {
-            // Create an SQL statement
-            Statement stmt = con.createStatement();
-
-            // Create string for SQL statement
-            String strSelect =
-                     "SELECT country.name, country.population "
-                    + "FROM country "
-                    + "ORDER BY country.population ASC";
-
-            // Execute SQL statement
-            ResultSet rset = stmt.executeQuery(strSelect);
-
-            // Return new country if valid.
-            // Check one is returned
-            if (rset.next())
-            {
-
-                Country country5 = new Country();
-                country5.name = rset.getString("name");
-                country5.population = rset.getInt("population");
-
-                return country5;
-            }
-            else
-                return null;
-        }
-        catch (Exception e)
-        {
-            System.out.println(e.getMessage());
-            System.out.println("Failed to get country");
-            return null;
-        }
-    }*/
-
-    public static void main(String[] args)
-    {
-        // Create new Application
-        App a = new App();
-
-        // Connect to database
-        a.connect();
-
-        //print UseCase1
-        ArrayList<Country> country = a.getUseCase1("Africa");
-        a.printUseCase1(country);
-
-        ArrayList<Country> country5 = a.getUseCase5();
-        a.printUseCase5(country5);
-
-
-
-
-        // Disconnect from database
-        a.disconnect();
     }
 
     public void printUseCase1(ArrayList<Country> country)
@@ -226,19 +127,21 @@ public class App
         }
     }
 
-    public void printUseCase5(ArrayList<Country> country) {
-        if (country == null) {
-            System.out.println("No countries found.");
-            return;
-        }
-        // Print header
-        System.out.println(String.format("%-20s %-12s", "Name", "Population"));
-        // Loop over all countries in the list
-        for (Country c1 : country) {
-            String c1_string = String.format("%-20s %,-12d", // Verwenden Sie , zur Formatierung der Population mit Tausender-Trennzeichen
-                    c1.name, c1.population);
-            System.out.println(c1_string);
-        }
+    public static void main(String[] args)
+    {
+        // Create new Application
+        App a = new App();
+
+        // Connect to database
+        a.connect();
+
+        //print UseCase1
+        ArrayList<Country> country = a.getUseCase1("Africa");
+        a.printUseCase1(country);
+
+
+        // Disconnect from database
+        a.disconnect();
     }
 
 }
