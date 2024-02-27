0972fc36a2c8c0f1ff367f84e3dfde3e1edb756e
Pia Flechsberger <pia.2002@aol.de>
2024-02-27 12:18:37 +0000

    implemented usecase2

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -127,6 +127,61 @@ public class App
         }
     }
 
+    //Usecase 2
+    public ArrayList<Country> getUseCase2(String region)
+    {
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT country.name, country.region, country.population "
+                            + "FROM country "
+                            + "WHERE country.region = '" + region + "' "
+                            + "ORDER BY country.population DESC"; //sort from the largest to the smallest
+
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+
+            // Return new country if valid.
+            // Check one is returned
+            ArrayList<Country> country = new ArrayList<Country>();
+            while (rset.next())
+            {
+                Country country2 = new Country();
+                country2.name = rset.getString("name");
+                country2.region = rset.getString("region");
+                country2.population = rset.getInt("population");
+                country.add(country2);
+            }
+            return country;
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get all the countries in a region organised by largest population to smallest");
+            return null;
+        }
+    }
+
+    public void printUseCase2(ArrayList<Country> country)
+    {
+        if (country == null) {
+            System.out.println("No countries found.");
+            return;
+        }
+        // Print header
+        System.out.println(String.format("%-20s %-15s %-12s", "Name", "Region", "Population"));
+        // Loop over all countries in the list
+        for (Country c1 : country) {
+            String c1_string = String.format("%-20s %-15s %,-12d", // Verwenden Sie , zur Formatierung der Population mit Tausender-Trennzeichen
+                    c1.name, c1.region, c1.population);
+            System.out.println(c1_string);
+        }
+    }
+
     public static void main(String[] args)
     {
         // Create new Application
@@ -139,6 +194,9 @@ public class App
         ArrayList<Country> country = a.getUseCase1("Africa");
         a.printUseCase1(country);
 
+        //print UseCase2
+        ArrayList<Country> country2 = a.getUseCase2("Middle East");
+        a.printUseCase2(country2);
 
         // Disconnect from database
         a.disconnect();
