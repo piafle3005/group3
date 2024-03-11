6177f2db8a9f52ba517f05792f8bdbef73fd9167
Pia Flechsberger <pia.2002@aol.de>
2024-03-11 12:05:03 +0000

    implemented Unit Test UseCase4-8

--- src/test/java/com/napier/sem/AppTest.java
+++ src/test/java/com/napier/sem/AppTest.java
@@ -17,6 +17,9 @@ public class AppTest {
         app = new App();
     }
 
+    /**
+     * Tests UseCase1
+     */
     @Test
     void printUseCase1TestNull()
     {
@@ -50,6 +53,9 @@ public class AppTest {
         app.printUseCase1(countries);
     }
 
+    /**
+     * Tests UseCase2
+     */
     @Test
     void printUseCase2TestNull()
     {
@@ -83,6 +89,9 @@ public class AppTest {
         app.printUseCase2(countries);
     }
 
+    /**
+     * Tests UseCase3
+     */
     @Test
     void printUseCase3TestNull()
     {
@@ -115,6 +124,121 @@ public class AppTest {
         app.printUseCase3(countries);
     }
 
+    /**
+     * UseCase4 has the same print method like UseCase1
+     */
+
+    /**
+     * Tests UseCase5
+     */
+    @Test
+    void printUseCase5TestNull()
+    {
+        app.printUseCase5(null);
+    }
+
+    @Test
+    void printUseCase5TestEmpty()
+    {
+        ArrayList<Country> countries = new ArrayList<Country>();
+        app.printUseCase5(countries);
+    }
+
+    @Test
+    void printUseCase5TestContainsNull()
+    {
+        ArrayList<Country> countries = new ArrayList<Country>();
+        countries.add(null);
+        app.printUseCase5(countries);
+    }
+
+    @Test
+    void printUseCase5TestValidData()
+    {
+        ArrayList<Country> countries = new ArrayList<Country>();
+        Country c1 = new Country();
+        c1.name= "Germany";
+        c1.population= 83200000;
+        countries.add(c1);
+        app.printUseCase5(countries);
+    }
+
+    /**
+     * UseCase6 has the same print method like UseCase2
+     */
+
+    /**
+     * Test UseCase7
+     */
+
+    @Test
+    void printUseCase7TestNull()
+    {
+        app.printUseCase7(null);
+    }
+
+    @Test
+    void printUseCase7TestEmpty()
+    {
+        ArrayList<City> cities = new ArrayList<City>();
+        app.printUseCase7(cities);
+    }
+
+    @Test
+    void printUseCase7TestContainsNull()
+    {
+        ArrayList<City> cities = new ArrayList<City>();
+        cities.add(null);
+        app.printUseCase7(cities);
+    }
+
+    @Test
+    void printUseCase7TestValidData()
+    {
+        ArrayList<City> cities = new ArrayList<City>();
+        City c1 = new City();
+        c1.name= "Edinburgh";
+        c1.population= 450180;
+        cities.add(c1);
+        app.printUseCase7(cities);
+    }
+
+    /**
+     * Test UseCase8
+     */
+
+    @Test
+    void printUseCase8TestNull()
+    {
+        app.printUseCase8(null);
+    }
+
+    @Test
+    void printUseCase8TestEmpty()
+    {
+        ArrayList<City> cities = new ArrayList<City>();
+        app.printUseCase8(cities);
+    }
+
+    @Test
+    void printUseCase8TestContainsNull()
+    {
+        ArrayList<City> cities = new ArrayList<City>();
+        cities.add(null);
+        app.printUseCase8(cities);
+    }
+
+    @Test
+    void printUseCase8TestValidData()
+    {
+        ArrayList<City> cities = new ArrayList<City>();
+        City c1 = new City();
+        c1.name= "Munich";
+        c1.continent= "Europe";
+        c1.population= 1194560;
+        cities.add(c1);
+        app.printUseCase8(cities);
+    }
 
 }
 
