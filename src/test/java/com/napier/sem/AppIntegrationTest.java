409ebcef35484dfc165ddaa445d7406e71941fd3
Pia Flechsberger <pia.2002@aol.de>
2024-04-09 16:03:28 +0100

    implement AppIntegrationTest 1-8

--- src/test/java/com/napier/sem/AppIntegrationTest.java
+++ src/test/java/com/napier/sem/AppIntegrationTest.java
@@ -20,5 +20,98 @@ public class AppIntegrationTest
 
     }
 
+    @Test
+    void printUseCase1TestValidData()
+    {
+        /**
+         *Test what happens under normal condition
+         */
+        ArrayList<Country> countries = new ArrayList<Country>();
+        Country c1 = new Country();
+        c1.name= "Germany";
+        c1.continent = "Europe";
+        c1.population= 83200000;
+        countries.add(c1);
+        app.printUseCase1(countries);
+        app.getUseCase1("Europe");
+        assertEquals(c1.name, "Germany");
+        assertEquals(c1.continent, "Europe");
+        assertEquals(c1.population, 83200000);
+    }
+
+    @Test
+    void printUseCase2TestValidData()
+    {
+        ArrayList<Country> countries = new ArrayList<Country>();
+        Country c1 = new Country();
+        c1.name= "Germany";
+        c1.region = "Western Europe";
+        c1.population= 82164700;
+        countries.add(c1);
+        app.printUseCase2(countries);
+        app.getUseCase2("Western Europe");
+        assertEquals(c1.name, "Germany");
+        assertEquals(c1.region, "Western Europe");
+        assertEquals(c1.population, 82164700);
+    }
+
+    @Test
+    void printUseCase3TestValidData()
+    {
+        ArrayList<Country> countries = new ArrayList<Country>();
+        Country c1 = new Country();
+        c1.name= "Germany";
+        c1.population = 82164700;
+        countries.add(c1);
+        app.printUseCase3(countries);
+        app.getUseCase3(3);
+        assertEquals(c1.name, "Germany");
+        assertEquals(c1.population, 82164700);
+    }
+
+    @Test
+    void printUseCase5TestValidData()
+    {
+        ArrayList<Country> countries = new ArrayList<Country>();
+        Country c1 = new Country();
+        c1.name= "Germany";
+        c1.population= 83200000;
+        countries.add(c1);
+        app.printUseCase5(countries);
+        app.getUseCase5();
+        assertEquals(c1.name, "Germany");
+        assertEquals(c1.population, 83200000);
+    }
+
+    @Test
+    void printUseCase7TestValidData()
+    {
+        ArrayList<City> cities = new ArrayList<City>();
+        City c1 = new City();
+        c1.name= "Edinburgh";
+        c1.population= 450180;
+        cities.add(c1);
+        app.printUseCase7(cities);
+        app.getUseCase7();
+        assertEquals(c1.name, "Edinburgh");
+        assertEquals(c1.population, 450180);
+    }
+
+    @Test
+    void printUseCase8TestValidData()
+    {
+        ArrayList<City> cities = new ArrayList<City>();
+        City c1 = new City();
+        c1.name= "Munich";
+        c1.continent= "Europe";
+        c1.population= 1194560;
+        cities.add(c1);
+        app.printUseCase8(cities);
+        app.getUseCase9("Germany");
+        assertEquals(c1.name, "Munich");
+        assertEquals(c1.continent, "Europe");
+        assertEquals(c1.population, 1194560);
+    }
+
     
 }
